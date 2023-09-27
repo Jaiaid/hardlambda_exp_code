122e7e062cd286a5afdac9c247cf4d379d48a362
Jaiaid <jaiaidmobin@gmail.com>
2023-09-27 11:40:20 -0400

    redis server binded to numa memnode

--- exp_ddl_share/system1/node2_pool.sh
+++ exp_ddl_share/system1/node2_pool.sh
@@ -3,8 +3,8 @@
 ROOT_DIR=..
 
 # numa node 0 pool
-redis-server $ROOT_DIR/system1/redis2.conf &
-time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/data_provider_redis.py -conf $ROOT_DIR/system1/redis2.conf -p 26379 &
+time numactl --cpunodebind=0 --membind=0  redis-server $ROOT_DIR/system1/redis2.conf &
+python3 $ROOT_DIR/data_provider_redis.py -conf $ROOT_DIR/system1/redis2.conf -p 26379 &
 # numa node 1 pool
-redis-server $ROOT_DIR/system1/redis3.conf &
-time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/data_provider_redis.py -conf $ROOT_DIR/system1/redis3.conf -p 26380 &
+time numactl --cpunodebind=0 --membind=0 redis-server $ROOT_DIR/system1/redis3.conf &
+python3 $ROOT_DIR/data_provider_redis.py -conf $ROOT_DIR/system1/redis3.conf -p 26380 &
