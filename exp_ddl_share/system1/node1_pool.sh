952a59fbdb51c9871fe2ea4d36291869ca1d62b6
Jaiaid <jaiaidmobin@gmail.com>
2023-09-27 10:59:33 -0400

    some more fix

--- exp_ddl_share/system1/node1_pool.sh
+++ exp_ddl_share/system1/node1_pool.sh
@@ -3,4 +3,5 @@
 ROOT_DIR=..
 
 # numa node 1 pool
+redis-server $ROOT_DIR/system1/redis1.conf &
 time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/data_provider_redis.py -conf $ROOT_DIR/system1/redis1.conf -p 26379 &
