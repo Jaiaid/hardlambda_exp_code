fef07e56a5fbc46cc1d9d4af0e7201248b6a2bd9
Jaiaid <jaiaidmobin@gmail.com>
2023-09-27 01:38:42 -0400

    a four process numa ethernet distributed system

---
+++ exp_ddl_share/system1/node1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PROF_TOOL_DIR=~/application/proftools/pcm/build/bin/
+
+# numa node 0
+time numactl --cpunodebind=0 --membind=0 python3 main.py -bs 16 -e 5 -nc 10 -n 2 -ddl -ip 129.21.22.239 -if eno2 -id 0 -ws 4 -m toy -d cifar10 -s local_random &
+# numa node 1
+time numactl --cpunodebind=1 --membind=1 python3 main.py -bs 16 -e 5 -nc 10 -n 2 -ddl -ip 129.21.22.239 -if eno2 -id 1 -ws 4 -m toy -d cifar10 -s local_random &
+
+# collect memory data at 2s interval
+sudo $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data.csv &
+# collect numa data at 2s interval
+sudo $PROF_TOOL_DIR/pcm-numa 2 -csv=numa_data.csv
