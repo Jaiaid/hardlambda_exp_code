339545368723906a8d7e92ef82832d1be0a47889
Jaiaid <jaiaidmobin@gmail.com>
2023-09-27 10:00:24 -0400

    added process for the distributed pool training

--- exp_ddl_share/trainer_process.py
+++ exp_ddl_share/trainer_process.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms
 from torch.nn.parallel import DistributedDataParallel as DDP
 
-from dataset import LocalPool, SharedPool, SharedRedisPool, DatasetPipeline
+from dataset import LocalPool, SharedPool, SharedRedisPool, SharedDistRedisPool, DatasetPipeline
 
 
 class ToyModel(nn.Module):
@@ -65,6 +65,8 @@ def get_training_process(strategy_name):
         return train_process_shared_pool_far
     elif strategy_name == "local_random":
         return train_process_local_pool_distrib_shuffle
+    elif strategy_name == "distributed_random":
+        return train_process_pool_distrib_shuffle
     return None
 
 def get_model(name:str) -> torch.nn.Module:
@@ -109,6 +111,54 @@ def train_process_local_pool_distrib_shuffle(rank, batch_size, epoch_count, num_
     print("creating data pipeline")
     # Define the transformations for data preprocessing
     dataset_pipeline = DatasetPipeline(LocalPool(dataset_name=dataset_name), batch_size=batch_size, sampler="dist", num_replicas=num_replicas)
+    import time
+    total_time = 0
+    count = 0
+    for epoch in range(epoch_count):
+        print("starting training epoch {0}...".format(epoch))
+        dataset_pipeline.set_epoch(epoch)
+        print("Memory rss footprint of process ", rank, " at epoch", epoch, " start", (psutil.Process().memory_info().rss)>>20, "MiB")
+        print("Memory shared footprint of process ", rank, " at epoch", epoch, " start", (psutil.Process().memory_info().shared)>>20, "MiB")
+        try:
+            for i, data in enumerate(dataset_pipeline):
+                inputs, labels = data
+                # generate label and move to GPU
+                if not gpu:
+                    one_hot = torch.nn.functional.one_hot(labels, num_classes=num_classes).float()
+                else:
+                    one_hot = torch.nn.functional.one_hot(labels, num_classes=num_classes).cuda(torch.device('cuda', device_idx))
+                # train one iteration
+                t = time.time()
+                training_pipeline.run_train_step(inputs=inputs, labels=one_hot)
+                total_time += time.time() - t
+                count += 1
+        except KeyboardInterrupt as e:
+            print(total_time/count)
+            exit(0)
+
+        print("Memory rss footprint of process ", rank, " at epoch", epoch, " end ", (psutil.Process().memory_info().rss)>>20, "MiB")
+        print("Memory shared footprint of process ", rank, " at epoch", epoch, " start", (psutil.Process().memory_info().shared)>>20, "MiB")
+
+def train_process_pool_distrib_shuffle(rank, batch_size, epoch_count, num_classes, dataset_name, model_name, num_replicas=None, ddl=None, gpu=False):
+    print("creating model pipeline")
+    # create the model training pipeline
+    training_pipeline = ModelPipeline(model=get_model(model_name))
+    # to select which gpu to use if multiple gpu
+    device_idx = rank if torch.cuda.device_count()>rank else 0
+
+    if gpu:
+        training_pipeline.make_gpu(device_idx)
+    if ddl:
+        print("creating process group")
+        dist.init_process_group("gloo", rank=rank, world_size=num_replicas)
+        print("making model distributed")
+        training_pipeline.make_distributed()
+    else:
+        dist.init_process_group("gloo", rank=rank, world_size=num_replicas)
+
+    print("creating data pipeline")
+    # Define the transformations for data preprocessing
+    dataset_pipeline = DatasetPipeline(SharedDistRedisPool(dataset_name=dataset_name), batch_size=batch_size, sampler="dist", num_replicas=num_replicas)
     import time
     total_time = 0
     count = 0
