d9f5340afb1841d610fa5c9dd6fea96674550fa2
Jaiaid <jaiaidmobin@gmail.com>
2023-09-27 11:39:52 -0400

    for now redis server will not strat from this because of unknown bug in starting

--- exp_ddl_share/data_provider_redis.py
+++ exp_ddl_share/data_provider_redis.py
@@ -43,26 +43,26 @@ if __name__=='__main__':
 
     try:
         # Start the Redis server as a subprocess
-        redis_server_process = subprocess.Popen([redis_server_cmd, args.conf_file]
-        , stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        # redis_server_process = subprocess.Popen([redis_server_cmd, args.conf_file]
+        # , stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
         # Wait for Redis to start (adjust the sleep duration as needed)
         time.sleep(2)
         data_pool = SharedDataRedis(args.port)
         # sleep idefinitely until keyboard exception
-        print("Memory rss footprint of redis process ", (psutil.Process(redis_server_process.pid).memory_info().rss)>>20, "MiB")
-        print("Memory shared footprint of redis starter process ", (psutil.Process().memory_info().shared)>>20, "MiB")
+        # print("Memory rss footprint of redis process ", (psutil.Process(redis_server_process.pid).memory_info().rss)>>20, "MiB")
+        # print("Memory shared footprint of redis starter process ", (psutil.Process().memory_info().shared)>>20, "MiB")
 
         print("Press Ctl+C to exit")
         while True:
-            out, err = redis_server_process.communicate()
-            print(out)
-            exit_code = redis_server_process.wait()
-            print("redis server exited with exit code {0}".format(exit_code))
+            # out, err = redis_server_process.communicate()
+            # print(out)
+            # exit_code = redis_server_process.wait()
+            # print("redis server exited with exit code {0}".format(exit_code))
             break
     except Exception as e:
         # kill the process
-        redis_server_process.kill()
+        # redis_server_process.kill()
         print(e)
         print("exiting")
 
