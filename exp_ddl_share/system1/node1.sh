923205ed6b0d96715cccc119048ce96214814e77
Jaiaid <jaiaidmobin@gmail.com>
2023-10-17 20:45:49 -0400

    metadata cache server ip and port added

--- exp_ddl_share/system1/node1.sh
+++ exp_ddl_share/system1/node1.sh
@@ -3,9 +3,9 @@ PROF_TOOL_DIR=~/application/proftools/pcm/build/bin/
 ROOT_DIR=..
 
 # numa node 0
-time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -ddl -ip 129.21.22.239 -if eno2 -id 0 -ws 4 -m toy -d cifar10 -s distributed_random &
+time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -ddl -ip 129.21.22.239 -if eno2 --mip 129.21.22.239 --mport 26379 -id 0 -ws 4 -m toy -d cifar10 -s distributed_random &
 # numa node 1
-time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -ddl -ip 129.21.22.239 -if eno2 -id 1 -ws 4 -m toy -d cifar10 -s distributed_random &
+time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -ddl -ip 129.21.22.239 -if eno2 --mip 129.21.22.239 --mport 26379 -id 1 -ws 4 -m toy -d cifar10 -s distributed_random &
 
 # collect memory data at 2s interval
 sudo $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data.csv &
--- exp_ddl_share/system1/node2.sh
+++ exp_ddl_share/system1/node2.sh
@@ -2,9 +2,9 @@
 PROF_TOOL_DIR=~/application/proftools/pcm/build/bin/
 ROOT_DIR=..
 # numa node 0
-time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -gpu -ddl -ip 129.21.22.239 -if eno3 -id 2 -ws 4 -m toy -d cifar10 -s distributed_random &
+time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -gpu -ddl -ip 129.21.22.239 -if eno3 --mip 129.21.22.239 --mport 26379 -id 2 -ws 4 -m toy -d cifar10 -s distributed_random &
 # numa node 1
-time numactl --cpunodebind=1 --membind=1  python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -gpu -ddl -ip 129.21.22.239 -if eno3 -id 3 -ws 4 -m toy -d cifar10 -s distributed_random &
+time numactl --cpunodebind=1 --membind=1  python3 $ROOT_DIR/main.py -bs 16 -e 5 -nc 10 -n 2 -gpu -ddl -ip 129.21.22.239 -if eno3 --mip 129.21.22.239 --mport 26379 -id 3 -ws 4 -m toy -d cifar10 -s distributed_random &
 
 # collect memory data at 2s interval
 sudo $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data.csv &
