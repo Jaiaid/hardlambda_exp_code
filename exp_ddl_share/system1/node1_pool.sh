0eeed863bb0179d366268accca0fd1982acc959e
Jaiaid <jaiaidmobin@gmail.com>
2023-10-25 00:28:09 -0400

    script update according to addition of imagenet and cmd line

--- exp_ddl_share/system1/node1_pool.sh
+++ exp_ddl_share/system1/node1_pool.sh
@@ -4,4 +4,4 @@ ROOT_DIR=..
 
 # numa node 1 pool
 time numactl --cpunodebind=1 --membind=1 redis-server $ROOT_DIR/system1/redis1.conf &
-python3 $ROOT_DIR/data_provider_redis.py -conf $ROOT_DIR/system1/redis1.conf -p 26379 &
+python3 $ROOT_DIR/data_provider_redis.py -data imagenet -root /sandbox1/data/imagenet/2017/ -conf $ROOT_DIR/system1/redis1.conf -p 26379 &
