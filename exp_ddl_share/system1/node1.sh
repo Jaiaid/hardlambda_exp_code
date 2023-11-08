b3730d62459514b3b94ba3a6926ed25b596e1678
Jaiaid <jaiaidmobin@gmail.com>
2023-11-08 03:24:25 -0500

    timeout in data collection

--- exp_ddl_share/system1/node1.sh
+++ exp_ddl_share/system1/node1.sh
@@ -8,8 +8,8 @@ time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e 10 
 time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/main.py -bs 16 -e 10 -nc 1000 -n 2 -ddl -ip 129.21.22.239 --port 44144 -if eno2 -id 1 -ws 4 -m resnet18 -d imagenet -s distributed_random -gpu -sampler graddist &
 
 # collect memory data at 2s interval
-sudo $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data_node2.csv &
+sudo timeout 3600 $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data_node2.csv &
 # collect numa data at 2s interval
-sudo $PROF_TOOL_DIR/pcm-numa 2 -csv=numa_data_node2.csv &
+sudo timeout 3600 $PROF_TOOL_DIR/pcm-numa 2 -csv=numa_data_node2.csv &
 # collect network data at 2s interval
-sudo sar -n DEV 2 > net_node1.csv
+sudo timeout 3600 sar -n DEV 2 > net_node1.csv
