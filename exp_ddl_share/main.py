1251031cb4cf4605c27fa9954d39fe859f065665
Jaiaid <jaiaidmobin@gmail.com>
2023-11-20 22:45:37 -0500

    add_argument() call fix

--- exp_ddl_share/main.py
+++ exp_ddl_share/main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("-ipm", "--ip_mover", type=str, help="data move service ip", default="lo", required=False)
     parser.add_argument("-portm", "--port_mover", type=str, help="data move service port", default="lo", required=False)
     # if epoch timing profile
-    parser.add_argument("-eprof", "--epoch_prof", type=bool, action="store_true",
+    parser.add_argument("-eprof", "--epoch_prof", action="store_true",
     help="will only run one epoch to report timing data of different portion in rank<rank>.timedata file", default=False, required=False)
     
     # get arguments
