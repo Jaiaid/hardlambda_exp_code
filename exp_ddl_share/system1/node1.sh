8f07b28ecefc8d8990394d5d1421cdac189312a3
Jaiaid <jaiaidmobin@gmail.com>
2023-11-29 17:28:25 -0500

    node1.sh fix

--- exp_ddl_share/system1/node1.sh
+++ exp_ddl_share/system1/node1.sh
@@ -15,8 +15,8 @@ time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e $EP
 time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/main.py -bs 16 -e $EPOCH -nc 1000 -n 2 -ddl -ip 10.21.12.239 --port 44144 -if eno2 -id 1 -ws 4 -m resnet18 -d imagenet -s distributed_random -gpu -sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 &
 
 # collect memory data at 2s interval for 10 min
-sudo timeout $PROF_TIME $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data_${SAMPLER}_node2.csv &
+sudo timeout $PROF_TIME $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data_${SAMPLER}_node1.csv &
 # collect numa data at 2s interval for 10 min
-sudo timeout $PROF_TIME $PROF_TOOL_DIR/pcm-numa 2 -csv=numa_data_${SAMPLER}_node2.csv &
+sudo timeout $PROF_TIME $PROF_TOOL_DIR/pcm-numa 2 -csv=numa_data_${SAMPLER}_node1.csv &
 # collect network data at 2s interval for 10 min
-sudo timeout $PROF_TIME sar -n DEV 2 > net_data_${SAMPLER}_node2.csv
+sudo timeout $PROF_TIME sar -n DEV 2 > net_data_${SAMPLER}_node1.csv
