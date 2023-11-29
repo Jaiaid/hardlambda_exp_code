c92280f4bd4806bf8f9824e63c0abad097318348
Jaiaid <jaiaidmobin@gmail.com>
2023-11-29 17:47:02 -0500

    eprof epoch limit removed

--- exp_ddl_share/system1/node1.sh
+++ exp_ddl_share/system1/node1.sh
@@ -10,9 +10,9 @@ PROF_TIME=$((MIN_PER_EPOCH_TIME_SEC*EPOCH))
 # the interface should be changed according to which interface we will be using
 # here the private network 10.21.12.0/24 is on eno2
 # numa node 0
-time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e $EPOCH -nc 1000 -n 2 -ddl -ip 10.21.12.239 --port 44144 -if eno2 -id 0 -ws 4 -m resnet18 -d imagenet -s distributed_random -gpu -sampler $SAMPLER -ipm 127.0.0.1 -portm 50524 &
+time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/main.py -bs 16 -e $EPOCH -nc 1000 -n 2 -ddl -ip 10.21.12.239 --port 44144 -if eno2 -id 0 -ws 4 -m resnet18 -d imagenet -s distributed_random -gpu -sampler $SAMPLER -ipm 127.0.0.1 -portm 50524 -eprof &
 # numa node 1
-time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/main.py -bs 16 -e $EPOCH -nc 1000 -n 2 -ddl -ip 10.21.12.239 --port 44144 -if eno2 -id 1 -ws 4 -m resnet18 -d imagenet -s distributed_random -gpu -sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 &
+time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/main.py -bs 16 -e $EPOCH -nc 1000 -n 2 -ddl -ip 10.21.12.239 --port 44144 -if eno2 -id 1 -ws 4 -m resnet18 -d imagenet -s distributed_random -gpu -sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 -eprof &
 
 # collect memory data at 2s interval for 10 min
 sudo timeout $PROF_TIME $PROF_TOOL_DIR/pcm-memory 2 -csv=memory_data_${SAMPLER}_node1.csv &
