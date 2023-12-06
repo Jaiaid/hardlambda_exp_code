8bfc4befcb3f3d53f25ad40019ff29814e2f2db6
Jaiaid <jaiaidmobin@gmail.com>
2023-12-06 01:54:00 -0500

    float loop variable fix

--- exp_ddl_share/trainer_process.py
+++ exp_ddl_share/trainer_process.py
@@ -180,7 +180,7 @@ def train_process_pool_distrib_shuffle(rank, batch_size, epoch_count, num_classe
         print("Memory shared footprint of process ", rank, " at epoch", epoch, " start", (psutil.Process().memory_info().shared)>>20, "MiB")
         try:
             epoch_start_time = time.time()
-            for i in range(len(dataset)/(4*batch_size)):
+            for i in range(int(len(dataset)//(4*batch_size))):
                 t = time.time()
                 data = next(iter(dataset_pipeline))
                 inputs, labels = data
