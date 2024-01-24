e8860290ee7c7b44f0f45ec26383a1be642a32ae
Jaiaid <jaiaidmobin@gmail.com>
2024-01-24 16:03:42 -0500

    spark processing exp script initial run success

--- exp_pyspark/etl_job.py
+++ exp_pyspark/etl_job.py
@@ -1,42 +1,86 @@
-
-# Import SparkSession
-from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext
-
+import time
+import subprocess
 import os
-os.environ["SPARK_HOME"] = r"/opt/spark/spark-3.5.0-bin-hadoop3"
-#os.environ["PYSPARK_SUBMIT_ARGS"] = "--master spark://10.21.12.222:19489 --total-executor 2 pyspark-shell"
-#os.environ["JAVA_HOME"] = r"/usr/bin"
+from pyspark.sql import SparkSession
 
-# Create SparkSession
-#spark = SparkSession.builder \
-#      .master("spark://10.21.12.222:19489") \
-#      .appName("SparkByExamples.com") \
-#      .getOrCreate()
-master_url = "spark://10.21.22.222:19489"  # Replace with the actual master IP and port
+from dataset import SharedRedisPool, SharedDistRedisPool, DatasetPipeline
+from DistribSampler import DistAwareDistributedSampler, DefaultDistributedSampler, GradualDistAwareDistributedSampler, GradualDistAwareDistributedSamplerBG
+from shade_modified import ShadeDataset, ShadeSampler
+from DataMovementService import DataMoverServiceInterfaceClient
 
-# Create a Spark configuration and context
-conf = SparkConf().setAppName("MySparkApplication").setMaster(master_url)
-spark = SparkContext(conf=conf)
+def create_spark_session():
+    return SparkSession.builder.appName("MemoryPressureJob").getOrCreate()
 
+def generate_large_dataset(spark, num_partitions=4, partition_size=10**6):
+    # Generate a large dataset with specified number of partitions and partition size
+    data = [i for i in range(num_partitions * partition_size)]
+    rdd = spark.sparkContext.parallelize(data, numSlices=num_partitions)
+    return rdd.map(lambda x: (x % num_partitions, x))
 
-# Create RDD from parallelize
-dataList = [("Java", 20000), ("Python", 100000), ("Scala", 3000)]
-rdd=spark.sparkContext.parallelize(dataList)
+def main():
+    # Create a Spark session
+    spark = create_spark_session()
 
-data = [('James','','Smith','1991-04-01','M',3000),
-  ('Michael','Rose','','2000-05-19','M',4000),
-  ('Robert','','Williams','1978-09-05','M',4000),
-  ('Maria','Anne','Jones','1967-12-01','F',4000),
-  ('Jen','Mary','Brown','1980-02-17','F',-1)
-]
+    rank = 0
+    dataset = SharedDistRedisPool()
+    data_sampler = GradualDistAwareDistributedSamplerBG(
+        dataset=dataset, num_replicas=2, batch_size=16)
+    data_sampler.set_rank(rank=0)
+    # starting the background data mover service
+    data_mover_service = subprocess.Popen(
+        """python3 {2}/DataMovementService.py --seqno {0}
+        -bs 16 -cn 10.21.12.239 26379 10.21.12.239 26380 10.21.12.222 26379 -pn 10.21.12.239 10.21.12.222 -p {1}""".format(
+            rank if rank < 3 else 2, 50524, os.path.dirname(os.path.abspath(__file__))).split()
+    )
+    # check if running
+    if data_mover_service.poll() is None:
+        print("data mover service is running")
+    # try 10 times to connect
+    connection_refused_count = 0
+    while connection_refused_count < 10: 
+        try:
+            data_mover = DataMoverServiceInterfaceClient("127.0.0.1", 50524)
+            break
+        except ConnectionError as e:
+            connection_refused_count += 1
+            print("connection establish attempt {0} failed".format(connection_refused_count))
+            # sleep for a second
+            time.sleep(1)
 
-columns = ["firstname","middlename","lastname","dob","gender","salary"]
-df = spark.createDataFrame(data=data, schema = columns)
+    if connection_refused_count == 10:
+        print("connection failed, exiting...")
+        data_mover_service.kill()
+        exit(1)
+    else:
+        print("connection successful after {0} attempt".format(connection_refused_count))
+        print("data movement service client interface is opened")
 
-df.createOrReplaceTempView("PERSON_DATA")
-for i in range(1000000):
-    df2 = spark.sql("SELECT * from PERSON_DATA")
-    df2.collect()
-df2.printSchema()
-df2.show()
+    # create the pipeline from sampler
+    dataset_pipeline = DatasetPipeline(dataset=dataset, batch_size=16,
+                                       sampler=data_sampler, num_replicas=2)
+
+    try:
+        # Generate a large dataset
+        large_dataset = spark.sparkContext.parallelize(dataset_pipeline, numSlices=2)#generate_large_dataset(spark)
+
+        # Perform some transformations to put pressure on memory
+        processed_data = (
+            large_dataset
+            .map(lambda x: x/255.0)  # Transform data
+            .groupByKey()  # Introduce shuffling operation
+            .mapValues(lambda values: sum(values))  # Another transformation
+        )
+
+        # Trigger an action to materialize the results and put pressure on memory
+        result = processed_data.collect()
+
+        # Print the first few results
+        print(result[:10])
+
+    finally:
+        # Stop the Spark session
+        spark.stop()
+
+if __name__ == "__main__":
+    main()
+
