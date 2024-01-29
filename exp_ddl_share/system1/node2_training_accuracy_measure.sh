43a1cba28b4d6aa250c8611e89d7634368c48011
Jaiaid <jaiaidmobin@gmail.com>
2024-01-29 14:05:25 -0500

    training accuracy measure script, script renamed

---
+++ exp_ddl_share/system1/node2_training_accuracy_measure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ROOT_DIR=..
+SAMPLER=default
+ARCH=resnet18
+EPOCH=10
+
+PROF_TIME=$((MIN_PER_EPOCH_TIME_SEC*EPOCH))
+
+# the interface should be changed according to which interface we will be using
+# here the private network 10.21.12.0/24 is on eno4
+# numa node 0
+time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/model_accuracy_measure.py --arch $ARCH -b 16 --epochs $EPOCH --world-size 2 --evaluate --dist-url tcp://10.21.12.239:44144 --rank 2 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50524 > rank0_accuracy_$ARCH.log  &
+# numa node 1
+time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/model_accuracy_measure.py --arch $ARCH -b 16 --epochs $EPOCH --world-size 2 --evaluate --dist-url tcp://10.21.12.239:44144 --rank 3 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 > rank1_accuracy_$ARCH.log  &
