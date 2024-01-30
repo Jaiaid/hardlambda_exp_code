2c683ea68bbce8cf12b0188b36ba7295793e1899
Jaiaid <jaiaidmobin@gmail.com>
2024-01-30 10:25:28 -0500

    script will not fork completely

--- exp_ddl_share/system1/node2_training_accuracy_measure.sh
+++ exp_ddl_share/system1/node2_training_accuracy_measure.sh
@@ -11,4 +11,4 @@ PROF_TIME=$((MIN_PER_EPOCH_TIME_SEC*EPOCH))
 # numa node 0
 time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/model_accuracy_measure.py --arch $ARCH -b 16 --epochs $EPOCH --world-size 2 --evaluate --dist-url tcp://10.21.12.239:44144 --rank 2 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50524 > rank0_accuracy_$ARCH.log  &
 # numa node 1
-time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/model_accuracy_measure.py --arch $ARCH -b 16 --epochs $EPOCH --world-size 2 --evaluate --dist-url tcp://10.21.12.239:44144 --rank 3 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 > rank1_accuracy_$ARCH.log  &
+time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/model_accuracy_measure.py --arch $ARCH -b 16 --epochs $EPOCH --world-size 2 --evaluate --dist-url tcp://10.21.12.239:44144 --rank 3 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 > rank1_accuracy_$ARCH.log
