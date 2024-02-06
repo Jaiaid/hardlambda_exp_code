9225c5eaa12f1ec1b533d06ddf4dabb6eb17cf80
Jaiaid <jaiaidmobin@gmail.com>
2024-02-06 16:44:29 -0500

    validation early exit

--- exp_ddl_share/model_accuracy_measure.py
+++ exp_ddl_share/model_accuracy_measure.py
@@ -454,7 +454,7 @@ def validate(val_loader, model, criterion, args):
 
                 if i % args.print_freq == 0:
                     progress.display(i + 1)
-                if i >= 5000:
+                if i >= 312:
                     break
 
     batch_time = AverageMeter('Time', ':6.3f', Summary.NONE)
