1aaada78de576b68d20101ce616b0e30871d1a12
Jaiaid <jaiaidmobin@gmail.com>
2024-03-05 16:49:22 -0500

    removed unnecessary classes

--- exp_pyspark/DistribSampler.py
+++ exp_pyspark/DistribSampler.py
@@ -1,14 +1,19 @@
-from typing import TypeVar, Optional, Iterator
 import copy
 import numpy as np
 import torch.distributed as dist
-from torch.utils.data import Dataset
 import time
 import math
+import subprocess
+import os
+
+from typing import TypeVar, Optional, Iterator
+from torch.utils.data import Dataset
+from torch.utils.data.distributed import DistributedSampler
+
+from DataMovementService import DataMoverServiceInterfaceClient
+
 
 T_co = TypeVar('T_co', covariant=True)
-
-from torch.utils.data.distributed import DistributedSampler
 
 class DefaultDistributedSampler(DistributedSampler):
     r"""Sampler that restricts data loading to a subset of the dataset.
@@ -47,7 +52,7 @@ class DefaultDistributedSampler(DistributedSampler):
                 fout.write(str(batch_no) + "," + str(read_freq) + "\n")
 
 
-class DistAwareDistributedSampler(DistributedSampler):
+class GradualDistAwareDistributedSamplerBG(DistributedSampler):
     r"""Sampler that restricts data loading to a subset of the dataset.
 
     
@@ -55,165 +60,9 @@ class DistAwareDistributedSampler(DistributedSampler):
 
     def __init__(self, dataset: Dataset, num_replicas: Optional[int] = None,
                  rank: Optional[int] = None, shuffle: bool = True,
-                 seed: int = 0, drop_last: bool = False, batch_size: int = 16) -> None:
+                 seed: int = 0, drop_last: bool = False, batch_size: int = 16,
+                 ip_mover=None, port_mover=None) -> None:
         super().__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
-        self.batch_size = batch_size
-        self.data_batch_read_latency = [0] * int(self.num_samples*num_replicas)
-        self.data_batch_read_freq = [0] * int(self.num_samples*num_replicas)
-        # self.rank_cache = redis.StrictRedis(host=metadata_cache_ip, port=metadata_cache_port, db=0)
-
-        # read the whole dataset and make a ranking
-        benchmarking_time_start = time.time()
-        time_list = []
-        total_read_time = 0
-        for i in range(0, len(dataset), batch_size):
-            t = time.time()
-            # we are only checking how much time to read from memory
-            for j in range(i, i+batch_size):
-                input = dataset[i]
-                total_read_time += time.time() - t
-            time_list.append(total_read_time)
-            total_read_time = 0
-        
-        self.batch_dist_ranking_list = list(numpy.argsort(time_list))
-        benchmarking_time_end = time.time()
-        print("benchmarking took {0}s".format(benchmarking_time_end - benchmarking_time_start))
-
-    def __iter__(self) -> Iterator[T_co]:
-        total_batch = len(self.batch_dist_ranking_list)
-        num_batch_per_replica = int(self.total_size / (self.num_replicas * self.batch_size))
-        start_idx = (self.epoch * num_batch_per_replica) % total_batch
-        # we are not modding this for batch generation logic simplification
-        end_idx = (start_idx + num_batch_per_replica)
-
-        # create indices array
-        indices = []
-        for i in range(start_idx, end_idx):
-            idx = i % total_batch
-            indices += list(
-                range(
-                    self.batch_dist_ranking_list[idx] * self.batch_size,
-                    self.batch_dist_ranking_list[idx] * self.batch_size + self.batch_size
-                )
-            )
-
-        # record the data's access frequency
-        # still not read but we are passing the indices so it should be read
-        for index in indices:
-            batch_no = int(index/self.batch_size)
-            self.data_batch_read_freq[batch_no] += 1
-
-        return iter(indices)
-
-    def set_batch_time(self, batch_no:int, read_time:float):
-        idx = self.rank*self.num_replicas + batch_no
-
-    def get_batch_time(self, batch_no:int) -> float:
-        key = self.rank*self.num_replicas + batch_no
-        return 0
-
-    def dump_data_read_freq(self, output_file_path):
-        r"""dump the data access freuqncy in text to given output file path
-        
-        Args:
-            output_file_path: output file where the data will be dumped
-        """
-        with open(output_file_path, "w") as fout:
-            fout.write("batch,frequency\n")
-            for batch_no, read_freq in enumerate(self.data_batch_read_freq):
-                fout.write(str(batch_no) + "," + str(read_freq) + "\n")
-
-
-class GradualDistAwareDistributedSampler(DistributedSampler):
-    r"""Sampler that restricts data loading to a subset of the dataset.
-
-    
-    """
-
-    def __init__(self, dataset: Dataset, num_replicas: Optional[int] = None,
-                 rank: Optional[int] = None, shuffle: bool = True,
-                 seed: int = 0, drop_last: bool = False, batch_size: int = 16) -> None:
-        super().__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
-        self.batch_size = batch_size
-        batch_count = 0
-        for i in range(0, len(dataset), batch_size):
-            batch_count += 1
-        self.data_batch_read_latency = [np.nan] * int(batch_count)
-        self.data_batch_read_freq = [0] * int(batch_count)
-        self.batch_dist_ranking_list = list(np.argsort(self.data_batch_read_latency))
-        
-    def set_epoch(self, epoch: int) -> None:
-        # sort at the beginning of epoch after first epoch
-        if epoch > 0:
-            self.batch_dist_ranking_list = list(np.argsort(self.data_batch_read_latency))
-        return super().set_epoch(epoch)
-
-    def __iter__(self) -> Iterator[T_co]:
-        total_batch = len(self.batch_dist_ranking_list)
-        num_batch_per_replica = int(self.total_size / (self.num_replicas * self.batch_size))
-        start_idx = (self.epoch * num_batch_per_replica) % total_batch
-        # we are not modding this for batch generation logic simplification
-        end_idx = (start_idx + num_batch_per_replica)
-
-        if self.epoch >= 1:
-            indices = []
-            for i in range(start_idx, end_idx):
-                idx = i % total_batch
-                indices += list(
-                    range(
-                        self.batch_dist_ranking_list[idx] * self.batch_size,
-                        self.batch_dist_ranking_list[idx] * self.batch_size + self.batch_size
-                    )
-                )
-        else:
-            # for first batch work like default
-            # from the 2nd batch we will have some information on data access latency
-            iterator = super().__iter__()
-            iter_copy = copy.deepcopy(iterator)
-            for index in iterator:
-                batch_no = int(index/self.batch_size)
-                self.data_batch_read_freq[batch_no] += 1
-            return iter_copy
-
-
-        # record the data's access frequency
-        # still not read but we are passing the indices so it should be read
-        for index in indices:
-            batch_no = int(index/self.batch_size)
-            self.data_batch_read_freq[batch_no] += 1
-
-        return iter(indices)
-
-    def set_batch_time(self, batch_no:int, read_time:float):
-        idx = batch_no
-        self.data_batch_read_latency[idx] = read_time
-
-    def get_batch_time(self, batch_no:int) -> float:
-        idx = batch_no
-        return self.data_batch_read_latency[idx]
-
-    def dump_data_read_freq(self, output_file_path):
-        r"""dump the data access freuqncy in text to given output file path
-        
-        Args:
-            output_file_path: output file where the data will be dumped
-        """
-        with open(output_file_path, "w") as fout:
-            fout.write("batch,frequency\n")
-            for batch_no, read_freq in enumerate(self.data_batch_read_freq):
-                fout.write(str(batch_no) + "," + str(read_freq) + "\n")
-
-
-class GradualDistAwareDistributedSamplerBG():
-    r"""Sampler that restricts data loading to a subset of the dataset.
-
-    
-    """
-
-    def __init__(self, dataset: Dataset, num_replicas: Optional[int] = None,
-                 rank: Optional[int] = None, shuffle: bool = True,
-                 seed: int = 0, drop_last: bool = False, batch_size: int = 16) -> None:
-        #super().__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
         self.batch_size = batch_size
         self.total_batch = math.ceil(len(dataset)/batch_size)
         batch_count = 0
@@ -222,9 +71,29 @@ class GradualDistAwareDistributedSamplerBG():
         self.data_batch_read_latency = [np.nan] * int(batch_count)
         self.data_batch_read_freq = [0] * int(batch_count)
         # needed to provide index from appropriate offset
-        self.rank = 0
-        self.total_size = len(dataset)
-        self.num_replicas = 2
+        self.rank = rank
+
+        # starting the background data mover service
+        self.data_mover_service = subprocess.Popen(
+            """python3 {2}/DataMovementService.py --seqno {0}
+            -bs 16 -cn 10.21.12.239 26379 10.21.12.239 26380 10.21.12.222 26379 -pn 10.21.12.239 10.21.12.222 -p {1}""".format(
+                rank if rank < 3 else 2, port_mover, os.path.dirname(os.path.abspath(__file__))).split()
+        )
+        # check if running
+        if self.data_mover_service.poll() is None:
+            print("data mover service is running")
+
+        # try 10 times to connect
+        connection_refused_count = 0
+        while connection_refused_count < 10: 
+            try:
+                self.data_mover = DataMoverServiceInterfaceClient(ip_mover, port_mover)
+                break
+            except ConnectionError as e:
+                connection_refused_count += 1
+                print("connection establish attempt {0} failed".format(connection_refused_count))
+                # sleep for a second
+                time.sleep(1)
 
     def set_epoch(self, epoch: int) -> None:
         return super().set_epoch(epoch)
@@ -242,17 +111,17 @@ class GradualDistAwareDistributedSamplerBG():
 
         for index in indices:
             batch_no = int(index/self.batch_size)
-            self.data_batch_read_freq[batch_no] += 1
-
+#            self.data_batch_read_freq[batch_no] += 1
+        
         return iter(indices)
 
     def set_batch_time(self, batch_no:int, read_time:float):
         idx = batch_no
-        self.data_batch_read_latency[idx] = read_time
+#        self.data_batch_read_latency[idx] = read_time
 
     def get_batch_time(self, batch_no:int) -> float:
         idx = batch_no
-        return self.data_batch_read_latency[idx]
+#        return self.data_batch_read_latency[idx]
 
     def dump_data_read_freq(self, output_file_path):
         r"""dump the data access freuqncy in text to given output file path
@@ -264,3 +133,19 @@ class GradualDistAwareDistributedSamplerBG():
             fout.write("batch,frequency\n")
             for batch_no, read_freq in enumerate(self.data_batch_read_freq):
                 fout.write(str(batch_no) + "," + str(read_freq) + "\n")
+
+    def __exit__(self):
+        # cleanup
+        try:
+            self.data_mover.close()
+            self.data_mover_service.kill()
+        except Exception as e:
+            print(e)
+
+    def __del__(self):
+        # cleanup
+        try:
+            self.data_mover.close()
+            self.data_mover_service.kill()
+        except Exception as e:
+            print(e)
