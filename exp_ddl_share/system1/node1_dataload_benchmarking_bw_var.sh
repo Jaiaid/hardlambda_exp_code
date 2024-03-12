dd0d3c6d44805c5bc2c6e2659827ebdd3d842703
Jaiaid <jaiaidmobin@gmail.com>
2024-03-12 12:53:47 -0400

    bw variation experiment script

---
+++ exp_ddl_share/system1/node1_dataload_benchmarking_bw_var.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ROOT_DIR=..
+SAMPLER=graddistbg
+ARCH=resnet18
+EPOCH=4
+BWMBPS=100
+BS=16
+
+sudo tc qdisc add dev eno2 root netem rate ${BWMBPS}mbit
+# the interface should be changed according to which interface we will be using
+# here the private network 10.21.12.0/24 is on eno2
+# numa node 0
+time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/dataload_benchmarking.py --arch $ARCH -b $BS --epochs $EPOCH --world-size 4 --dist-url tcp://10.21.12.239:44144 -if eno2 --rank 0 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50524 > rank0_bwvar_$ARCH_$SAMPLER_$BS_$BWMBPS.log  &
+# numa node 1
+time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/dataload_benchmarking.py --arch $ARCH -b $BS --epochs $EPOCH --world-size 4 --dist-url tcp://10.21.12.239:44144 -if eno2 --rank 1 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 > rank1_bwvar_$ARCH_$SAMPLER_$BS_$BWMBPS.log
---
+++ exp_ddl_share/system1/node2_dataload_benchmarking_bw_var.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ROOT_DIR=..
+SAMPLER=graddistbg
+ARCH=resnet18
+EPOCH=4
+BWMBPS=100
+BS=16
+
+sudo tc qdisc add dev eno4 root netem rate ${BWMBPS}mbit
+# the interface should be changed according to which interface we will be using
+# here the private network 10.21.12.0/24 is on eno4
+# numa node 0
+time numactl --cpunodebind=0 --membind=0 python3 $ROOT_DIR/dataload_benchmarking.py --arch $ARCH -b $BS --epochs $EPOCH --world-size 4 --dist-url tcp://10.21.12.239:44144 -if eno4 --rank 2 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50524 > rank2_bwvar_$ARCH_$SAMPLER_$BS_$BWMBPS.log  &
+# numa node 1
+time numactl --cpunodebind=1 --membind=1 python3 $ROOT_DIR/dataload_benchmarking.py --arch $ARCH -b $BS --epochs $EPOCH --world-size 4 --dist-url tcp://10.21.12.239:44144 -if eno4 --rank 3 --sampler $SAMPLER -ipm 127.0.0.1 -portm 50525 > rank3_bwvar_$ARCH_$SAMPLER_$BS_$BWMBPS.log
