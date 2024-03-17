db7289409ed3e91be4c067bcb15f390b63f2adcf
Jaiaid <jaiaidmobin@gmail.com>
2024-03-17 18:17:26 -0400

    data train scripts file package

---
+++ datatrain/DistribSampler.py
@@ -0,0 +1,150 @@
+import copy
+import numpy as np
+import torch.distributed as dist
+import time
+import math
+import subprocess
+import os
+
+from typing import TypeVar, Optional, Iterator
+from torch.utils.data import Dataset
+from torch.utils.data.distributed import DistributedSampler
+
+from DataMovementService import DataMoverServiceInterfaceClient
+
+
+T_co = TypeVar('T_co', covariant=True)
+
+class DefaultDistributedSampler(DistributedSampler):
+    r"""Sampler that restricts data loading to a subset of the dataset.
+
+    
+    """
+
+    def __init__(self, dataset: Dataset, num_replicas: Optional[int] = None,
+                 rank: Optional[int] = None, shuffle: bool = True,
+                 seed: int = 0, drop_last: bool = False, batch_size: int = 16) -> None:
+        super().__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
+        self.batch_size = batch_size
+        self.data_batch_read_latency = [0] * int(self.num_samples*num_replicas)
+        self.data_batch_read_freq = [0] * int(self.num_samples*num_replicas)
+
+    def __iter__(self) -> Iterator[T_co]:
+        iterator = super().__iter__()
+        iter_copy = copy.deepcopy(iterator)
+        for index in iterator:
+            batch_no = int(index/self.batch_size)
+            self.data_batch_read_freq[batch_no] += 1
+        return iter_copy
+
+    def set_batch_time(self, batch_no:int, read_time:float):
+        pass
+
+    def dump_data_read_freq(self, output_file_path):
+        r"""dump the data access freuqncy in text to given output file path
+        
+        Args:
+            output_file_path: output file where the data will be dumped
+        """
+        with open(output_file_path, "w") as fout:
+            fout.write("batch,frequency\n")
+            for batch_no, read_freq in enumerate(self.data_batch_read_freq):
+                fout.write(str(batch_no) + "," + str(read_freq) + "\n")
+
+
+class GradualDistAwareDistributedSamplerBG():
+    r"""Sampler that restricts data loading to a subset of the dataset.
+
+    
+    """
+
+    def __init__(self, dataset: Dataset, num_caches: Optional[int] = None,
+                 rank: Optional[int] = None, shuffle: bool = True, batch_size: int = 16,
+                 ip_mover=None, port_mover=None) -> None:
+        self.num_caches = num_caches
+        self.batch_size = batch_size
+        self.dataset = dataset
+        self.total_size = len(dataset)
+        self.total_batch = math.ceil(len(dataset)/batch_size)
+        batch_count = 0
+        for i in range(0, len(dataset), batch_size):
+            batch_count += 1
+        self.data_batch_read_latency = [np.nan] * int(batch_count)
+        self.data_batch_read_freq = [0] * int(batch_count)
+        # needed to provide index from appropriate offset
+        self.rank = rank
+        self.epoch = 0
+
+        # starting the background data mover service
+        self.data_mover_service = subprocess.Popen(
+            """python3 {2}/DataMovementService.py --seqno {0}
+            -bs 16 -cn 10.21.12.239 26379 10.21.12.239 26380 10.21.12.222 26379 -pn 10.21.12.239 10.21.12.222 -p {1}""".format(
+                rank if rank < 3 else 2, port_mover, os.path.dirname(os.path.abspath(__file__))).split()
+        )
+        # check if running
+        if self.data_mover_service.poll() is None:
+            print("data mover service is running")
+
+        # try 10 times to connect
+        connection_refused_count = 0
+        while connection_refused_count < 10: 
+            try:
+                self.data_mover = DataMoverServiceInterfaceClient(ip_mover, port_mover)
+                break
+            except ConnectionError as e:
+                connection_refused_count += 1
+                print("connection establish attempt {0} failed".format(connection_refused_count))
+                # sleep for a second
+                time.sleep(1)
+
+    def set_epoch(self, epoch: int) -> None:
+        self.epoch = epoch
+ 
+    def set_rank(self, rank: int) -> None:
+        self.rank = rank
+
+    def __iter__(self) -> Iterator[T_co]:
+        num_batch_per_cache = int(self.total_size / (self.num_caches * self.batch_size))
+        # we are not modding this for batch generation logic simplification
+        end_idx = num_batch_per_cache
+
+        indices = list(range(self.rank * num_batch_per_cache * self.batch_size,
+                             self.rank * num_batch_per_cache * self.batch_size + end_idx * self.batch_size))
+        indices *= num_batch_per_cache
+        
+        return iter(indices)
+
+    def set_batch_time(self, batch_no:int, read_time:float):
+        idx = batch_no
+#        self.data_batch_read_latency[idx] = read_time
+
+    def get_batch_time(self, batch_no:int) -> float:
+        idx = batch_no
+#        return self.data_batch_read_latency[idx]
+
+    def dump_data_read_freq(self, output_file_path):
+        r"""dump the data access freuqncy in text to given output file path
+        
+        Args:
+            output_file_path: output file where the data will be dumped
+        """
+        with open(output_file_path, "w") as fout:
+            fout.write("batch,frequency\n")
+            for batch_no, read_freq in enumerate(self.data_batch_read_freq):
+                fout.write(str(batch_no) + "," + str(read_freq) + "\n")
+
+    def __exit__(self):
+        # cleanup
+        try:
+            self.data_mover.close()
+            self.data_mover_service.kill()
+        except Exception as e:
+            print(e)
+
+    def __del__(self):
+        # cleanup
+        try:
+            self.data_mover.close()
+            self.data_mover_service.kill()
+        except Exception as e:
+            print(e)
---
+++ datatrain/dataset.py
@@ -0,0 +1,180 @@
+import math
+import psutil
+import numpy as np
+import redis
+import ctypes
+import torch
+import torchvision
+import nvidia.dali.fn as fn
+import nvidia.dali.types as types
+from torch.utils.data import Dataset, DataLoader, DistributedSampler
+from nvidia.dali.plugin.pytorch import DALIGenericIterator
+from nvidia.dali import pipeline_def, Pipeline
+
+
+
+def get_dataset(dataset_name:str):
+    if dataset_name == "cifar10":
+        # Define the transformations for data preprocessing
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),               # Convert images to PyTorch tensors
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize to mean 0 and standard deviation 1
+        ])
+        return torchvision.datasets.CIFAR10(root="./data", download=True, transform=transform)
+    elif dataset_name == "cifar100":
+        # Define the transformations for data preprocessing
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),               # Convert images to PyTorch tensors
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize to mean 0 and standard deviation 1
+        ])
+
+        return torchvision.datasets.CIFAR100(root="./data", download=True, transform=transform)
+
+
+class SharedRedisPool(Dataset):
+    def __init__(self):
+        # prepare redis client
+        redis_host = 'localhost'  # Change this to your Redis server's host
+        redis_port = 6379  # Change this to your Redis server's port
+        self.redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+        self.nb_samples = int.from_bytes(self.redis_client.get("length"), 'little')
+    
+    def __getitem__(self, index):
+        deser_x = self.redis_client.get("data" + str(index))
+        deser_y = self.redis_client.get("label" + str(index))
+        x = np.frombuffer(deser_x, dtype=np.float32)
+        dim_x = int(math.ceil(math.sqrt(x.shape[0]/3)))
+        x = torch.from_numpy(x.reshape(3,dim_x,dim_x))
+        y = int.from_bytes(deser_y, 'little')
+
+        return x, y
+    
+    def __len__(self):
+        return self.nb_samples
+
+
+class SharedDistRedisPool(Dataset):
+    def __init__(self):
+        # prepare redis client
+        redis_host1 = '10.21.12.222'  # Change this to your Redis server's host
+        redis_port1 = 26379  # Change this to your Redis server's port
+        self.redis_client1 = redis.StrictRedis(host=redis_host1, port=redis_port1, db=0)
+        self.nb_samples = int.from_bytes(self.redis_client1.get("length"), 'little')
+
+        redis_host2 = '10.21.12.239'  # Change this to your Redis server's host
+        redis_port2 = 26379  # Change this to your Redis server's port
+        self.redis_client2 = redis.StrictRedis(host=redis_host2, port=redis_port2, db=0)
+        self.nb_samples += int.from_bytes(self.redis_client2.get("length"), 'little')
+
+        redis_host3 = '10.21.12.239'  # Change this to your Redis server's host
+        redis_port3 = 26380  # Change this to your Redis server's port
+        self.redis_client3 = redis.StrictRedis(host=redis_host3, port=redis_port3, db=0)
+        self.nb_samples += int.from_bytes(self.redis_client3.get("length"), 'little')
+
+        self.redis_query_stat = { '10.21.12.222:26379': 0, '10.21.12.239:26379': 0, '10.21.12.239:26380': 0}
+
+    def __getitem__(self, index):
+        if index >= 2*self.nb_samples/3:
+            index -= int(2*self.nb_samples/3)
+            select_redis_client = self.redis_client1
+            self.redis_query_stat['10.21.12.222:26379'] += 1
+        elif index >= self.nb_samples/3:
+            index -= int(self.nb_samples/3)
+            select_redis_client = self.redis_client2
+            self.redis_query_stat['10.21.12.239:26379'] += 1
+        else:
+            select_redis_client = self.redis_client3
+            self.redis_query_stat['10.21.12.239:26380'] += 1
+
+        deser_x = select_redis_client.get("data" + str(index))
+        deser_y = select_redis_client.get("label" + str(index))
+        x = np.frombuffer(deser_x, dtype=np.float32)
+        dim_x = int(math.ceil(math.sqrt(x.shape[0]/3)))
+        x = torch.from_numpy(x.reshape(3,dim_x,dim_x))
+        y = int.from_bytes(deser_y, 'little')
+
+        return x, y
+
+    def __len__(self):
+        return self.nb_samples
+
+    def get_query_stat(self):
+        return self.redis_query_stat
+
+
+class PyTorchDaliPipeline(Pipeline):
+    def __init__(self, pytorch_dataset, batch_size, num_threads, device_id):
+        super(PyTorchDaliPipeline, self).__init__(batch_size, num_threads, device_id, seed=12)
+        self.input = ops.PythonFunction(function=self.load_data, output_layout=[("data", types.FLOAT), ("label", types.INT32)])
+        self.pytorch_dataset = pytorch_dataset
+        self.batch_size = batch_size
+
+    def load_data(self):
+        # Use the PyTorch dataset to load data
+        for _ in range(self.batch_size):
+            sample = self.pytorch_dataset[np.random.randint(len(self.pytorch_dataset))]
+            yield (sample['data'], sample['label'])
+
+    def define_graph(self):
+        return self.input()
+
+
+class DatasetPipeline():
+    def __init__(self, dataset:Dataset, batch_size:int, sampler:DistributedSampler=None, num_replicas:int=None) -> None:
+        self.pipeline = None
+        self.sampler = sampler
+        if sampler is not None:
+            self.dataloader: DataLoader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, sampler=sampler)
+        elif sampler == "dali":
+            num_threads = int(psutil.cpu_count(logical=False)//2)
+            device_id = 0
+            self.dataset = dataset
+            self.pipeline = PyTorchDaliPipeline(
+                pytorch_dataset=dataset, batch_size=batch_size,
+                num_threads=num_threads, device_id=device_id)
+            dali_pipeline.build()
+        else:
+            self.dataloader: DataLoader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+
+    def set_epoch(self, epoch: int):
+        self.sampler.set_epoch(epoch)
+
+    def __iter__(self):
+        if self.sampler == "dali":
+            return DALIGenericIterator(
+                pipelines=[self.pipeline], output_map=["data", "label"],
+                size=len(self.dataset)
+            )
+        return self.dataloader._get_iterator()
+    
+    def __len__(self):
+        if self.sampler == "dali":
+            return len(self.dataset)
+        return len(self.dataloader)
+    
+    def dump_data_read_freq(self, output_file_path:str):
+        self.sampler.dump_data_read_freq(output_file_path=output_file_path)
+
+
+class GraddistBGPipeline():
+    def __init__(self, dataset:Dataset, batch_size:int, sampler:DistributedSampler=None, num_replicas:int=None) -> None:
+        print("dataset length: {0}".format(len(dataset)))
+        self.sampler = sampler
+        if sampler is not None:
+            self.dataloader: DataLoader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, sampler=sampler)
+        self.count = 0
+
+    def set_epoch(self, epoch: int):
+        self.sampler.set_epoch(epoch)
+
+    def __iter__(self):
+        idx = next(self.sampler.__iter__())
+        x, y = self.dataloader.dataset[idx]
+        
+        self.count += 1
+        if self.count % self.sampler.batch_size == 0 and idx>0:
+            self.sampler.data_mover.updatecache((idx-1)//self.sampler.batch_size)
+        yield x, y
+
+    def dump_data_read_freq(self, output_file_path:str):
+        self.sampler.dump_data_read_freq(output_file_path=output_file_path)
