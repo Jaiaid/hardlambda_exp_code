f80dd92bcb5a52389e19c4658ce6f797303bf6a7
Jaiaid <jaiaidmobin@gmail.com>
2024-03-18 13:18:21 -0400

    fix of cache creation script

--- exp_ddl_share/system1/node1_pool.sh
+++ exp_ddl_share/system1/node1_pool.sh
@@ -12,5 +12,5 @@ done
 time numactl --cpunodebind=0 --membind=0  redis-server $ROOT_DIR/exp_ddl_share/system1/redis2.conf &
 python3 $ROOT_DIR/cache_scripts/data_provider_redis.py -data cifar10 -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset 0 -conf $ROOT_DIR/exp_ddl_share/system1/redis2.conf -p 26379 &
 # numa node 1 pool
-time numactl --cpunodebind=0 --membind=0 redis-server $ROOT_DIR/exp_ddl_share/system1/redis3.conf &
+time numactl --cpunodebind=1 --membind=1 redis-server $ROOT_DIR/exp_ddl_share/system1/redis3.conf &
 python3 $ROOT_DIR/cache_scripts/data_provider_redis.py -data cifar10 -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset $CACHE_SIZE -conf $ROOT_DIR/exp_ddl_share/system1/redis3.conf -p 26380 &
--- exp_ddl_share/system1/node2_pool.sh
+++ exp_ddl_share/system1/node2_pool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_DIR=..
+ROOT_DIR=../..
 CACHE_SIZE=15000
 
 # kill the existing redis-server
@@ -9,5 +9,5 @@ for pid in `lsof -i -P -n | grep "redis-ser" | awk -F' ' '{ print $2 }' | tail -
 done
 
 # numa node 1 pool
-time numactl --cpunodebind=1 --membind=1 redis-server $ROOT_DIR/system1/redis1.conf &
-python3 $ROOT_DIR/data_provider_redis.py -data cifar10 -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset $((CACHE_SIZE+CACHE_SIZE)) -conf $ROOT_DIR/system1/redis1.conf -p 26379 &
+time numactl --cpunodebind=0 --membind=0 redis-server $ROOT_DIR/system1/redis1.conf &
+python3 $ROOT_DIR/cache_scripts/data_provider_redis.py -data cifar10 -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset $((CACHE_SIZE+CACHE_SIZE)) -conf $ROOT_DIR/system1/redis1.conf -p 26379 &
