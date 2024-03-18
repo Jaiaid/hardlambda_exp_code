a26c4e928ddebc78c69da27623aef742023993f9
Jaiaid <jaiaidmobin@gmail.com>
2024-03-18 11:10:30 -0400

    create cache for cifar dataset

--- exp_ddl_share/system1/node1_pool.sh
+++ exp_ddl_share/system1/node1_pool.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ROOT_DIR=..
-CACHE_SIZE=4000
+ROOT_DIR=../..
+CACHE_SIZE=15000
 
 # numa node 0 pool
-time numactl --cpunodebind=0 --membind=0  redis-server $ROOT_DIR/system1/redis2.conf &
-python3 $ROOT_DIR/data_provider_redis.py -data imagenet -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset 0 -conf $ROOT_DIR/system1/redis2.conf -p 26379 &
+time numactl --cpunodebind=0 --membind=0  redis-server $ROOT_DIR/exp_ddl_share/system1/redis2.conf &
+python3 $ROOT_DIR/cache_scripts/data_provider_redis.py -data cifar10 -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset 0 -conf $ROOT_DIR/exp_ddl_share/system1/redis2.conf -p 26379 &
 # numa node 1 pool
-time numactl --cpunodebind=0 --membind=0 redis-server $ROOT_DIR/system1/redis3.conf &
-python3 $ROOT_DIR/data_provider_redis.py -data imagenet -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset $CACHE_SIZE -conf $ROOT_DIR/system1/redis3.conf -p 26380 &
+time numactl --cpunodebind=0 --membind=0 redis-server $ROOT_DIR/exp_ddl_share/system1/redis3.conf &
+python3 $ROOT_DIR/cache_scripts/data_provider_redis.py -data cifar10 -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset $CACHE_SIZE -conf $ROOT_DIR/exp_ddl_share/system1/redis3.conf -p 26380 &
--- exp_ddl_share/system1/node2_pool.sh
+++ exp_ddl_share/system1/node2_pool.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ROOT_DIR=..
-CACHE_SIZE=4000
+CACHE_SIZE=15000
 
 # numa node 1 pool
 time numactl --cpunodebind=1 --membind=1 redis-server $ROOT_DIR/system1/redis1.conf &
-python3 $ROOT_DIR/data_provider_redis.py -data imagenet -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset $((CACHE_SIZE+CACHE_SIZE)) -conf $ROOT_DIR/system1/redis1.conf -p 26379 &
+python3 $ROOT_DIR/data_provider_redis.py -data cifar10 -root /sandbox1/data/imagenet/2017/ -size $CACHE_SIZE -offset $((CACHE_SIZE+CACHE_SIZE)) -conf $ROOT_DIR/system1/redis1.conf -p 26379 &
