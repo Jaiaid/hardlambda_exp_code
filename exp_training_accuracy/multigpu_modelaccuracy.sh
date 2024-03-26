494a07a507cf05abb0825c52f2b6616de1993fc7
Jaiaid <jaiaidmobin@gmail.com>
2024-03-26 00:58:23 -0400

    bash cmd fix

--- exp_training_accuracy/multigpu_modelaccuracy.sh
+++ exp_training_accuracy/multigpu_modelaccuracy.sh
@@ -10,7 +10,7 @@ for NETARCH in mobilenet_v2 resnet18 resnet50 efficientnet_b1 vgg16;do
         sed -i "s/SAMPLER/"$SAMPLER"/" system_2a4000_tmp.txt
         sed -i "s/BS/"$BS"/" system_2a4000_tmp.txt
         sed -i "s/EPOCH/"$EPOCH"/" system_2a4000_tmp.txt
-        $ROOT_DIR/job_distributor/master.sh system_2a4000_tmp.txt
+        bash $ROOT_DIR/job_distributor/master.sh system_2a4000_tmp.txt
         rm system_2a4000_tmp.txt
         sleep 30
     done
