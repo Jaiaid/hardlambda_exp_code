c68eb3d0e46c006ca91fcccd72d2703012017ba3
Jaiaid <jaiaidmobin@gmail.com>
2024-03-26 10:20:28 -0400

    bash script will run through bash invocation

--- exp_background/multigpu_dataload_benchmarking.sh
+++ exp_background/multigpu_dataload_benchmarking.sh
@@ -11,7 +11,7 @@ for NETARCH in mobilenet_v2 resnet18 resnet50 efficientnet_b1 vgg16;do
         sed -i "s/SAMPLER/"$SAMPLER"/" system_2a4000_1p1000_tmp.txt
         sed -i "s/BS/"$BS"/" system_2a4000_1p1000_tmp.txt
         sed -i "s/EPOCH/"$EPOCH"/" system_2a4000_1p1000_tmp.txt
-        $ROOT_DIR/job_distributor/master.sh system_2a4000_1p1000_tmp.txt
+        bash $ROOT_DIR/job_distributor/master.sh system_2a4000_1p1000_tmp.txt
         rm system_2a4000_1p1000_tmp.txt
     done
 done
