69086b96dd5d6e75174f385582c4fa4d0f298263
Jaiaid <jaiaidmobin@gmail.com>
2024-03-27 12:08:15 -0400

    better diagnosis and cmd format with better coverage

--- env_creation/envcreation.sh
+++ env_creation/envcreation.sh
@@ -3,6 +3,12 @@
 mkdir -p $1
 cd $1
 git clone https://Jaiaid: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@github.com/Jaiaid/hardlambda_exp_code
-virtualenv -m venv
-source venv/bin/activate
-pip install --no-cache-dir -r requirements.txt
+python3 -m virtualenv --python=python3 venv
+COMPLETIONCODE=$?
+
+if [[ $COMPLETIONCODE -eq 0 ]]
+then
+    source venv/bin/activate & python3 -m pip install --no-cache-dir -r hardlambda_exp_code/requirements.txt
+else
+    echo "virtualenv creation failed"
+fi
