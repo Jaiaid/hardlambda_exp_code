3dc4e635b9f05c91c03bae84f5f06f3b0df1fcbc
Jaiaid <jaiaidmobin@gmail.com>
2024-03-27 11:12:47 -0400

    private clone repo temp key added

--- env_creation/envcreation.sh
+++ env_creation/envcreation.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $1
 cd $1
-git clone https://github.com/Jaiaid/hardlambda_exp_code
+git clone https://Jaiaid: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@github.com/Jaiaid/hardlambda_exp_code
 virtualenv -m venv
 source venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
