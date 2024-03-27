59da51084363e7a4f28b125c59ac3719b62cbd84
Jaiaid <jaiaidmobin@gmail.com>
2024-03-27 16:01:07 -0400

    removed earlier things in case multiple attempt

--- env_creation/envcreation.sh
+++ env_creation/envcreation.sh
@@ -2,7 +2,13 @@
 
 mkdir -p $1
 cd $1
+# if repo is there remove it
+# needed for multiple attempt
+rm -rf hardlambda_exp_code
 git clone https://Jaiaid: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@github.com/Jaiaid/hardlambda_exp_code
+# if venv already there remove it
+# needed in case of multiple run
+rm -rf venv
 python3 -m virtualenv --python=python3 venv
 COMPLETIONCODE=$?
 
