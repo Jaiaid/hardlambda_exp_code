c37ede205fefadefe265063747fc9d494fb109f5
Jaiaid <jaiaidmobin@gmail.com>
2024-03-28 21:58:53 -0400

    dataload benchmarking experiment modifications

---
+++ exp_background/multinode_dataload_benchmarking.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#!/bin/bash
+ROOT_DIR=..
+EPOCH=2
+
+for IMGDIM 32 64 128 256 512 1024;do
+    pushd $ROOT_DIR/cache_scripts
+    bash $ROOT_DIR/job_distributor/master.sh cachecreation_joblist_conf2.txt
+    # give some time to create the data cache
+    # we are experimenting with small so 1minute should suffice
+    sleep 60
+    popd
+    for BS in 4 8 16 32 64;do
+        for NETARCH in mobilenet_v2 resnet18 resnet50 resnet101;do
+            for SAMPLER in default dali shade graddistbg;do
+                sed "s/NETARCH/"$NETARCH"/" system_2a4000_1p1000.txt > system_2a4000_1p1000_tmp.txt
+                sed -i "s/SAMPLER/"$SAMPLER"/" system_2a4000_1p1000_tmp.txt
+                sed -i "s/BS/"$BS"/" system_2a4000_1p1000_tmp.txt
+                sed -i "s/EPOCH/"$EPOCH"/" system_2a4000_1p1000_tmp.txtIMGDIM
+                sed -i "s/IMGDIM/"$IMGDIM"/" system_2a4000_1p1000_tmp.txt
+                bash $ROOT_DIR/job_distributor/master.sh system_2a4000_1p1000_tmp.txt
+                rm system_2a4000_1p1000_tmp.txt
+            done
+        done
+    done
+done 
--- exp_background/process.sh
+++ exp_background/process.sh
@@ -10,10 +10,17 @@ WORLD=$7
 DISTURL=$8
 IFACE=$9
 RANK=${10}
-DMOVERIP=${11}
-DMOVERPORT=${12}
+CACHEDESC=${11}
+IMGSIZE=${12}
 
 # to activate virtual environment
 source ../../venv/bin/activate
 
-time numactl --cpunodebind=$CPUNODE --membind=$MEMNODE python3 ../benchmarking_scripts/dataload_benchmarking.py -a $NETARCH -sampler $SAMPLER -b $BS --epochs $EPOCH --world-size $WORLD --dist-url $DISTURL -if $IFACE --rank $RANK -ipm $DMOVERIP -portm $DMOVERPORT
+if [[ $RANK -eq 0 ]]
+then
+    mkdir -p imagedim${IMGSIZE} 
+    time numactl --cpunodebind=$CPUNODE --membind=$MEMNODE python3 ../benchmarking_scripts/model_accuracy_measure.py -a $NETARCH -sampler $SAMPLER -b $BS --epochs $EPOCH --world-size $WORLD --dist-url $DISTURL -if $IFACE --rank $RANK -cachedesc $CACHEDESC > /dev/null
+    mv benchmark_iteration_step.csv imagedim${IMGSIZE}/benchmark_iteration_step_${SAMPLER}_${NETARCH}_${BS}.csv 
+else
+    time numactl --cpunodebind=$CPUNODE --membind=$MEMNODE python3 ../benchmarking_scripts/model_accuracy_measure.py -a $NETARCH -sampler $SAMPLER -b $BS --epochs $EPOCH --world-size $WORLD --dist-url $DISTURL -if $IFACE --rank $RANK -cachedesc $CACHEDESC  > /dev/null
+fi
