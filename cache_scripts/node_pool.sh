deaeb4d4262266a456fbd3b24fa4ac375f255cfd
Jaiaid <jaiaidmobin@gmail.com>
2024-03-29 14:53:09 -0400

    updated cache craetion script

--- cache_scripts/node_pool.sh
+++ cache_scripts/node_pool.sh
@@ -12,6 +12,7 @@ REDIS_PORT=$8
 SEQNO=$9
 CACHEDESC_FILE=${10}
 
+# set -x
 
 # kill the existing redis-server
 for pid in `lsof -i -P -n | grep "redis-ser" | awk -F' ' '{ print $2 }' | tail -n +2`;do
@@ -23,7 +24,7 @@ for pid in `lsof -i -P -n | grep "python3" | awk -F' ' '{ print $2 }'`;do
     kill $pid
 done
 
-sleep 10
+sleep 5
 
 # to activate virtual environment
 source ../../venv/bin/activate
@@ -31,9 +32,14 @@ source ../../venv/bin/activate
 sed "92s/port 6379/port "$REDIS_PORT"/" redis.conf > redis_out$REDIS_PORT.conf
 sed -i "88s/protected-mode yes/protected-mode no/" redis_out$REDIS_PORT.conf
 # numa node pool
-time numactl --cpunodebind=$CPU_NODE --membind=$MEM_NODE  redis-server redis_out$REDIS_PORT.conf &
-python3 $ROOT_DIR/cache_scripts/data_provider_redis.py -data $DATASET_TYPE -root $DATASETROOT -dim $IMAGEDIM -size $CACHE_SIZE -offset $OFFSET -conf redis_out.conf -p $REDIS_PORT &
-# give some time to create and occupy the port
-sleep 2
+echo "starting redis server"
+time numactl --cpunodebind=$CPU_NODE --membind=$MEM_NODE  redis-server redis_out$REDIS_PORT.conf > /dev/null &
+sleep 5
+echo "starting redis cache populating"
+python3 $ROOT_DIR/cache_scripts/data_provider_redis.py -data $DATASET_TYPE -root $DATASETROOT -dim $IMAGEDIM -size $CACHE_SIZE -offset $OFFSET -conf redis_out.conf -p $REDIS_PORT
+sleep 10
 # create the movement service
-python3 $ROOT_DIR/cache_scripts/DataMovementService.py --seqno $SEQNO -cdesc $CACHEDESC_FILE &
+echo "creating data movement service"
+python3 $ROOT_DIR/cache_scripts/DataMovementService.py --seqno $SEQNO -cdesc $CACHEDESC_FILE > cache$SEQNO.txt 2>&1 &
+echo "created data movement service"
+# set +x
