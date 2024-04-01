f67456ce9509cbcf375602cc676531b52ac072a5
Jaiaid <jaiaidmobin@gmail.com>
2024-04-01 09:35:52 -0400

    killing of python processes

--- exp_motivation/distributed_steps_benchmarking.sh
+++ exp_motivation/distributed_steps_benchmarking.sh
@@ -4,3 +4,6 @@ source ../../venv/bin/activate
 
 python3 nn_distributed_step_benchmarking.py --rank 0 --dist-url $1 &
 python3 nn_distributed_step_benchmarking.py --rank 1 --dist-url $1
+
+# to kill everything of python
+for pid in `lsof -i -P -n | grep "python3" | awk -F' ' '{ print $2 }' | tail -n +2`;do kill $pid;done
