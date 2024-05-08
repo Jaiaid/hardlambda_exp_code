f361cedcc987081400ccf20cf36fb2e42ed776db
Jaiaid <jaiaidmobin@gmail.com>
2024-05-08 17:40:38 -0400

    datasize vs and particular batch and image size phenomenon graph script

--- graph_generation/datasize_vs_dltime.py
+++ graph_generation/datasize_vs_dltime.py
@@ -4,7 +4,7 @@ import numpy as np
 import argparse
 
 COMPARED_SYSTEM_LABEL_DICT = {"default": "Default", "dali": "Dali", "shade": "SHADE", "graddistbg": "proposed"}
-NETWORK_NAMES_LABEL_DICT = {"mobilenet_v2": "MobileNet-V2", "resnet18": "ResNet-18", "resnet50": "ResNet-50", "resnet101": "ResNet-101"}
+NETWORK_NAMES_LABEL_DICT = {"mobilenet_v2": "MobileNet-V2", "resnet18": "ResNet18", "resnet50": "ResNet50", "resnet101": "ResNet101"}
 # hatchlist = ['\\', '\\', '\\', '\\', '/', '/', '/', '/', 'o', 'o', 'o', 'o', 'x', 'x', 'x', 'x']
 hatchlist = ['\\', '/', 'o', 'x']
 DATAFILE_PATH = "4a4000_alldata.tsv"
@@ -65,12 +65,11 @@ if __name__=="__main__":
             if bs not in data_dict[network_name][sampler]:
                 data_dict[network_name][sampler][bs] = {}
             if dim not in data_dict[network_name][sampler][bs]:
-                data_dict[network_name][sampler][bs][dim] = [0, 0, 0, 0]
+                data_dict[network_name][sampler][bs][dim] = [0, 0]
             try:
                 data_dict[network_name][sampler][bs][dim][0] = dl_time
-                data_dict[network_name][sampler][bs][dim][2] = dl_time + cu_time + (TRACE_DATA_DICT[network_name][dim][bs][0] + TRACE_DATA_DICT[network_name][dim][bs][1]) * bs 
             except Exception as e:
-                print(e)
+                pass
 
         # 2nd pass to normalize value w.r.t 4 worker data
         with open(DATAFILE_PATH) as fin:
@@ -90,28 +89,24 @@ if __name__=="__main__":
                 exec_time = float(tokens[8])
                 try:
                     data_dict[network_name][sampler][bs][dim][1] = data_dict[network_name]["default"][bs][dim][0]/dl_time
-                    data_dict[network_name][sampler][bs][dim][3] = data_dict[network_name]["default"][bs][dim][2]/(dl_time + cu_time + (TRACE_DATA_DICT[network_name][dim][bs][0] + TRACE_DATA_DICT[network_name][dim][bs][1]) * bs)
                 except Exception as e:
-                    print(e)
+                    pass
 
-    for dim in DIM_LIST:
-        for bs in BS_LIST:
-            # plot and axis object
-            fig, ax = plot.subplots(figsize=(4, 2.25))
-            # ax1, ax2 = ax.twinx()
-            xtick_labels = []
-            bias = 0.6/len(NETWORK_NAMES_LABEL_DICT) 
-            width = 0.15
+    
 
-            for i, sampler in enumerate(COMPARED_SYSTEM_LABEL_DICT):
-                try:
-                    datalist = [data_dict[network_name][sampler][bs][dim][1] for network_name in NETWORK_NAMES_LABEL_DICT]
-                    # print(i, sampler, datalist)
-                    offset = (i-2) * bias
-                    ax.bar(np.arange(0, len(COMPARED_SYSTEM_LABEL_DICT)) + offset, datalist,
-                        label=COMPARED_SYSTEM_LABEL_DICT[sampler], width=width, hatch=hatchlist[i])
-                except Exception as e:
-                    print(e)
+    fig, ax = plot.subplots(figsize=(4, 2.25))
+    ax.set_xscale("log")
+
+    for sampler in COMPARED_SYSTEM_LABEL_DICT:
+        plot_data = []
+        for network in NETWORK_NAMES_LABEL_DICT:
+            for bs in data_dict[network_name][sampler]:
+                for dim in data_dict[network_name][sampler][bs]:
+                    plot_data.append([bs*dim*dim*3/1000, data_dict[network_name][sampler][bs][dim][1]])
+                    if data_dict[network_name][sampler][bs][dim][1] < 0.25:
+                        print(network_name, sampler, dim, bs)
+        ax.scatter([elem[0] for elem in plot_data], [elem[1] for elem in plot_data], label=COMPARED_SYSTEM_LABEL_DICT[sampler])
+
             # print(np.arange(0, len(NETWORK_NAMES)))
             # set the legends and limit in y axis
             #ax.legend(ncol=3, loc="upper center", fontsize=6, frameon=False)
@@ -119,20 +114,20 @@ if __name__=="__main__":
             #ax.get_legend().get_frame().set_facecolor((1, 1, 1, 0.1))
             # ax.set_yscale("log")
 
-            plot.tick_params(axis='y', which='major', labelsize=6)
-            plot.xticks(fontweight='bold', fontsize=6)
-            plot.xticks(np.arange(0, len(NETWORK_NAMES_LABEL_DICT)), NETWORK_NAMES_LABEL_DICT)
+    # plot.tick_params(axis='y', which='major', labelsize=6)
+    # plot.xticks(fontweight='bold', fontsize=6)
+    # plot.xticks(np.arange(0, len(NETWORK_NAMES_LABEL_DICT)), NETWORK_NAMES_LABEL_DICT)
 
-            plot.tick_params(axis='y', which='major', labelsize=8)
-            plot.yticks(fontweight='bold', fontsize=8)
-            ax.set_yticks(np.arange(0, 2, 0.5), minor=False)
-            ax.set_yticks(np.arange(0, 2, 0.1), minor=True)
-            plot.grid(axis='y', which='major')
-            ax.set_ylim([0, 1.9])
-            # ax.set_xticklabels(BAR_LABELS, {'fontsize':8, 'fontweight': "bold"})
-            ax.set_ylabel("Normalized Speedup", {'fontsize':8, 'fontweight': "bold"})
-            ax.set_xlabel("", {'fontsize':8, 'fontweight': "bold"})
-            ax.legend(ncol=2, fontsize=8)
-            
-            fig.savefig("figures/exectime_bs{0}_is{1}.png".format(bs, dim), dpi=600, bbox_inches="tight")
-            # fig.savefig("exectime_bs{0}_is{1}.png".format(bs, dim), format="pdf", bbox_inches="tight")
+    # plot.tick_params(axis='y', which='major', labelsize=8)
+    # plot.yticks(fontweight='bold', fontsize=8)
+    ax.set_yticks(np.arange(0.0, 9, 1), minor=False)
+    ax.set_yticks(np.arange(0.0, 9, 0.5), minor=True)
+    plot.grid(axis='y', which='both')
+    # ax.set_ylim([0.6, 1.5])
+    # ax.set_xticklabels(BAR_LABELS, {'fontsize':8, 'fontweight': "bold"})
+    ax.set_ylabel("Normalized Speedup", {'fontsize':8, 'fontweight': "bold"})
+    ax.set_xlabel("Batch Data Size (KB)", {'fontsize':8, 'fontweight': "bold"})
+    ax.legend(fontsize=8)
+    
+    fig.savefig("fig_dltime_vs_datasize.png", dpi=600, bbox_inches="tight")
+    fig.savefig("fig_dltime_vs_datasize.pdf", format="pdf", bbox_inches="tight")
