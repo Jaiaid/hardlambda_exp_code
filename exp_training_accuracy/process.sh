fa35c3430f6112a98c9e0b691ffa41b96bea100e
Jaiaid <jaiaidmobin@gmail.com>
2024-09-13 16:04:17 -0400

    training accuracy process script udpate

--- exp_training_accuracy/process.sh
+++ exp_training_accuracy/process.sh
@@ -10,15 +10,29 @@ WORLD=$7
 DISTURL=$8
 IFACE=$9
 RANK=${10}
-DMOVERIP=${11}
-DMOVERPORT=${12}
+CACHEDESC=${11}
+IMGSIZE=${12}
+
+# kill previous job
+for pid in `lsof -i -P -n | grep ":44144" | awk -F' ' '{ print $2 }'`;do
+    kill $pid
+done
+sleep 5
 
 # to activate virtual environment
 source ../../venv/bin/activate
 
+set -x
+
 if [[ $RANK -eq 0 ]]
 then
-time numactl --cpunodebind=$CPUNODE --membind=$MEMNODE python3 ../benchmarking_scripts/model_accuracy_measure.py -a $NETARCH -sampler $SAMPLER -b $BS --epochs $EPOCH --world-size $WORLD --dist-url $DISTURL -if $IFACE --rank $RANK -ipm $DMOVERIP -portm $DMOVERPORT > rank0_accuracy_${NETARCH}_${SAMPLER}.log
+    mkdir -p imagedim${IMGSIZE} 
+    time numactl --cpunodebind=$CPUNODE --membind=$MEMNODE python3 ../benchmarking_scripts/dataload_benchmarking.py -a $NETARCH -sampler $SAMPLER -b $BS --epochs $EPOCH --world-size $WORLD --dist-url $DISTURL -if $IFACE --rank $RANK -cachedesc $CACHEDESC > t.log
+    touch imagedim${IMGSIZE}/benchmark_iteration_step_${SAMPLER}_${NETARCH}_${BS}.tsv 
+    tail -1 benchmark_iteration_step.tsv >> imagedim${IMGSIZE}/benchmark_iteration_step_${SAMPLER}_${NETARCH}_${BS}.tsv 
 else
-time numactl --cpunodebind=$CPUNODE --membind=$MEMNODE python3 ../benchmarking_scripts/model_accuracy_measure.py -a $NETARCH -sampler $SAMPLER -b $BS --epochs $EPOCH --world-size $WORLD --dist-url $DISTURL -if $IFACE --rank $RANK -ipm $DMOVERIP -portm $DMOVERPORT > /dev/null
+    time numactl --cpunodebind=$CPUNODE --membind=$MEMNODE python3 ../benchmarking_scripts/dataload_benchmarking.py -a $NETARCH -sampler $SAMPLER -b $BS --epochs $EPOCH --world-size $WORLD --dist-url $DISTURL -if $IFACE --rank $RANK -cachedesc $CACHEDESC > t_$RANK.log
 fi
+
+redis-cli -h 127.0.0.1 -p 6379 FLUSHDB
+set +x
